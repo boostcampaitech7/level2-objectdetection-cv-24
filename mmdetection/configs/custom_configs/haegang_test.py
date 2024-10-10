a71b029e7201003dacc754398bc7a39f8b698e92
lazely <lz0136@naver.com>
2024-10-10 14:15:20 +0900

    CO-DETR 빼고 다 됨

---
+++ mmdetection/configs/custom_configs/haegang_test.py
@@ -0,0 +1,134 @@
+_base_ = [
+    '../../projects/CO-DETR/configs/codino/co_dino_5scale_swin_l_lsj_16xb1_3x_coco.py'
+]
+
+dataset_type = 'CocoDataset'
+data_root = '../dataset/'
+classes = ('General trash', 'Paper', 'Paper pack', 'Metal', 'Glass',
+           'Plastic', 'Styrofoam', 'Plastic bag', 'Battery', 'Clothing')
+
+# 데이터 파이프라인 설정
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', scale=(1024, 1024), keep_ratio=True),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='PackDetInputs')
+]
+
+val_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='Resize', scale=(1024, 1024), keep_ratio=True),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='PackDetInputs')
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='Resize', scale=(1024, 1024), keep_ratio=True),
+    dict(type='PackDetInputs')
+]
+
+# 데이터 로더 설정
+train_dataloader = dict(
+    batch_size=2,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='train.json',
+        data_prefix=dict(img=''),
+        filter_cfg=dict(filter_empty_gt=True, min_size=32),
+        pipeline=train_pipeline,
+        metainfo=dict(classes=classes)
+    )
+)
+
+val_dataloader = dict(
+    batch_size=4,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='test.json',
+        data_prefix=dict(img=''),
+        test_mode=True,
+        pipeline=val_pipeline,
+        metainfo=dict(classes=classes)
+    )
+)
+
+test_dataloader = dict(
+    batch_size=2,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='test.json',
+        data_prefix=dict(img=''),
+        test_mode=True,
+        pipeline=test_pipeline,
+        metainfo=dict(classes=classes)
+    )
+)
+
+optim_wrapper = dict(
+    _delete_=True,
+    type='OptimWrapper',
+    optimizer=dict(type='AdamW', lr=2e-4, weight_decay=0.0001),
+    clip_grad=dict(max_norm=0.1, norm_type=2),
+    paramwise_cfg=dict(custom_keys={'backbone': dict(lr_mult=0.1)})
+)
+
+val_evaluator = dict(
+    type='CocoMetric',
+    ann_file=data_root + 'test.json',
+    metric='bbox',
+    format_only=False,
+    classwise=True
+)
+
+test_evaluator = dict(
+    type='CocoMetric',
+    ann_file=data_root + 'test.json',
+    metric='bbox',
+    format_only=False,
+    classwise=True
+)
+
+train_cfg = dict(
+    _delete_=True,
+    type='EpochBasedTrainLoop',
+    max_epochs=40,
+    val_interval=1
+)
+
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+
+param_scheduler = [
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=40,
+        by_epoch=True,
+        milestones=[11, 30],
+        gamma=0.1
+    )
+]
+
+work_dir = './work_dirs/codetr_swin_transformer'
+
+default_hooks = dict(
+    checkpoint=dict(type='CheckpointHook', interval=1, max_keep_ckpts=3),
+    logger=dict(type='LoggerHook', interval=50)
+)
+
+log_processor = dict(by_epoch=True)
--- mmdetection/train.py
+++ mmdetection/train.py
@@ -1,90 +1,52 @@
-# 모듈 import
-import wandb
-from mmcv import Config
-from mmdet.datasets import build_dataset
-from mmdet.models import build_detector
-from mmdet.apis import train_detector
-from mmdet.datasets import (build_dataloader, build_dataset,
-                            replace_ImageToTensor)
-from mmdet.utils import get_device
+import argparse
+import torch
+from mmengine.config import Config
+from mmengine.runner import Runner
+from mmdet.utils import register_all_modules 
 
-classes = ("General trash", "Paper", "Paper pack", "Metal", "Glass", 
-           "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing")
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train a detection model')
+    parser.add_argument('config', help='train config file path')
+    parser.add_argument('--work-dir', help='directory to save logs and models')
+    parser.add_argument('--resume-from', help='checkpoint file to resume from')
+    parser.add_argument('--seed', type=int, default=42, help='random seed')
+    parser.add_argument('--device', default='cuda', help='device used for training')
+    args = parser.parse_args()
+    return args
 
-# config file 들고오기
-cfg = Config.fromfile('./mmdetection/configs/cascade_rcnn/cascade_rcnn_r50_fpn_1x_coco.py')
+def main():
+    args = parse_args()
+    
+    # 모든 mmdetection 모듈을 등록
+    register_all_modules()
 
-root='./dataset/'
+    # config 파일 로드
+    cfg = Config.fromfile(args.config)
 
-# dataset config 수정
-cfg.data.train.classes = classes
-cfg.data.train.img_prefix = root
-cfg.data.train.ann_file = root + 'train.json' # train json 정보
-cfg.data.train.pipeline[2]['img_scale'] = (512,512) # Resize
+    # 작업 디렉토리 설정
+    if args.work_dir is not None:
+        cfg.work_dir = args.work_dir
 
-cfg.data.test.classes = classes
-cfg.data.test.img_prefix = root
-cfg.data.test.ann_file = root + 'test.json' # test json 정보
-cfg.data.test.pipeline[1]['img_scale'] = (512,512) # Resize
+    # 장치 설정
+    cfg.device = args.device
 
-cfg.data.samples_per_gpu = 4
+    # 학습 재개 설정
+    if args.resume_from:
+        cfg.resume = True
+        cfg.load_from = args.resume_from
+    else:
+        cfg.resume = False
+        cfg.load_from = None
 
-cfg.seed = 2022
-cfg.gpu_ids = [0]
-cfg.work_dir = './mmdetection/work_dirs/cascade_rcnn_r50_fpn_1x_coco_trash'
+    # 랜덤 시드 설정
+    cfg.seed = args.seed
 
-if isinstance(cfg.model.roi_head.bbox_head, list):
-    for head in cfg.model.roi_head.bbox_head:
-        head.num_classes = 10
-else:
-    cfg.model.roi_head.bbox_head.num_classes = 10
+    # 설정 출력 (디버깅용)
+    print(cfg.pretty_text)
 
-# cfg.model.roi_head.bbox_head.num_classes = 10
-# cfg.model.bbox_head.num_classes = 10 # Retinanet
+    # Runner 생성 및 학습 시작
+    runner = Runner.from_cfg(cfg)
+    runner.train()
 
-cfg.optimizer_config.grad_clip = dict(max_norm=35, norm_type=2)
-cfg.checkpoint_config = dict(max_keep_ckpts=3, interval=1)
-cfg.device = get_device()
-
-wandb.init(project='mmdetection-trash-detection', entity='gaemanssi2-naver-ai-boostcamp', name=cfg.model.type)
-
-wandb.config.update({
-    "model": cfg.model.type,
-    "backbone": cfg.model.backbone.type,
-    "learning_rate": cfg.optimizer.lr,
-    "batch_size": cfg.data.samples_per_gpu,
-    "num_epochs": cfg.runner.max_epochs,
-    "img_scale": cfg.data.train.pipeline[2]['img_scale'],
-    "classes": classes
-})
-
-if 'log_config' not in cfg:
-    cfg.log_config = dict(interval=50)
-if 'hooks' not in cfg.log_config:
-    cfg.log_config.hooks = []
-
-cfg.evaluation = dict(interval=1, metric='bbox')
-
-# build_dataset
-datasets = [build_dataset(cfg.data.train)]
-
-# dataset 확인
-datasets[0]
-
-# 모델 build 및 pretrained network 불러오기
-model = build_detector(cfg.model)
-model.init_weights()
-
-cfg.log_config.hooks.append(
-    dict(type='MMDetWandbHook',
-         init_kwargs={'project': 'mmdetection-trash-detection'},
-         interval=len(datasets[0]),
-         log_checkpoint=False,
-         log_checkpoint_metadata=False,
-         num_eval_images=0) # val dataset을 안쓰는 경우 0, 사용 시 100 정도로 숫자 조정
-)
-
-# 모델 학습
-train_detector(model, datasets[0], cfg, distributed=False, validate=False)
-
-wandb.finish()
+if __name__ == '__main__':
+    main()
