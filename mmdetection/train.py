e7dc700d621e60142235184ce1b43edd22292041
wonjeongjeong <wj3714@naver.com>
2024-10-16 14:51:51 +0900

    Add : wandb sweep 뼈대

--- mmdetection/train.py
+++ mmdetection/train.py
@@ -6,6 +6,8 @@ from mmdet.utils import register_all_modules
 from mmdet.registry import MODELS
 from mmengine.registry import MODELS as ENGINE_MODELS
 from mmdet.models import DetDataPreprocessor
+import wandb
+from wandb.integration.ultralytics import add_wandb_callback
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a detection model')
@@ -20,6 +22,13 @@ def parse_args():
 def main():
     args = parse_args()
 
+    wandb.init(project="wj3714-naver-ai-boostcamp-org", job_type="baseline")
+    
+    # Load sweep hyperparameters
+    sweep_config = wandb.config
+    learning_rate = sweep_config.learning_rate
+    batch_size = sweep_config.batch_size
+    max_epochs = sweep_config.max_epochs
     
     # 모든 mmdetection 모듈을 등록
     register_all_modules()
@@ -28,7 +37,9 @@ def main():
     # config 파일 로드
     cfg = Config.fromfile(args.config)
 
-    
+    cfg.optim_wrapper.optimizer.lr = learning_rate
+    cfg.train_dataloader.batch_size = batch_size
+    cfg.train_cfg.max_epochs = max_epochs
 
     # 작업 디렉토리 설정
     if args.work_dir is not None:
@@ -53,7 +64,9 @@ def main():
 
     # Runner 생성 및 학습 시작
     runner = Runner.from_cfg(cfg)
+    add_wandb_callback(runner.model, enable_model_checkpointing=True)
     runner.train()
 
+    wandb.finish()
 if __name__ == '__main__':
     main()
